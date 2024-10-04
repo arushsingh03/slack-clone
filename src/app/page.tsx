5c2d1e94374bce8120593c67aa80afa05721b9df
Arush Singh <sarushmad@gmail.com>
2024-10-04 18:44:42 +0530

    implimented basic sign in & sign up functionality along with ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import { AuthScreen } from "@/features/auth/components/auth-screen";
+
 export default function Home () {
   return (
-    <div className="text-rose-500 font-bold text-2xl">
-      Hello Arush     
-    </div>
+   <AuthScreen />
   )
 }
